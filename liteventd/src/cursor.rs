d53d82788abf9afee13d1ab0cd69bdbdf5f4997a
jonathan <jonathan.lapiquonne@gmail.com>
2025-07-26 19:13:40 -0400

    add vec reader

--- liteventd/src/cursor.rs
+++ liteventd/src/cursor.rs
@@ -3,7 +3,8 @@ use base64::{
     engine::{GeneralPurpose, general_purpose},
 };
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
+use thiserror::Error;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Order {
@@ -118,5 +119,204 @@ impl Args {
         } else {
             (self.first.unwrap_or(40), self.after.clone())
         }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ReadError {
+    #[error("{0}")]
+    Unknown(#[from] anyhow::Error),
+
+    #[error("ciborium.ser >> {0}")]
+    CiboriumSer(#[from] ciborium::ser::Error<std::io::Error>),
+
+    #[error("ciborium.de >> {0}")]
+    CiboriumDe(#[from] ciborium::de::Error<std::io::Error>),
+
+    #[error("base64 decode: {0}")]
+    Base64Decode(#[from] base64::DecodeError),
+}
+
+pub struct Reader<T> {
+    data: Vec<T>,
+    args: Args,
+    order: Order,
+}
+
+impl<T> Reader<T>
+where
+    T: Cursor + Clone,
+    T: Send + Unpin,
+    T: Bind<T = T>,
+{
+    pub fn new(data: Vec<T>) -> Self {
+        Self {
+            data,
+            args: Args::default(),
+            order: Order::Asc,
+        }
+    }
+
+    pub fn order(&mut self, order: Order) -> &mut Self {
+        self.order = order;
+
+        self
+    }
+
+    pub fn desc(&mut self) -> &mut Self {
+        self.order(Order::Desc)
+    }
+
+    pub fn args(&mut self, args: Args) -> &mut Self {
+        self.args = args;
+
+        self
+    }
+
+    pub fn backward(&mut self, last: u16, before: Option<Value>) -> &mut Self {
+        self.args(Args {
+            last: Some(last),
+            before,
+            ..Default::default()
+        })
+    }
+
+    pub fn forward(&mut self, first: u16, after: Option<Value>) -> &mut Self {
+        self.args(Args {
+            first: Some(first),
+            after,
+            ..Default::default()
+        })
+    }
+
+    pub fn execute(&self) -> Result<ReadResult<T>, ReadError> {
+        let is_order_desc = matches!(
+            (&self.order, self.args.is_backward()),
+            (Order::Asc, true) | (Order::Desc, false)
+        );
+
+        let mut data = self.data.clone().into_iter().collect::<Vec<_>>();
+        T::sort_by(&mut data, is_order_desc);
+        let (limit, cursor) = self.args.get_info();
+
+        if let Some(cursor) = cursor.as_ref() {
+            let cursor = T::deserialize_cursor(cursor)?;
+            T::retain(&mut data, cursor, is_order_desc);
+        }
+
+        let data_len = data.len();
+        data = data.into_iter().take((limit + 1).into()).collect();
+
+        let has_more = data_len > data.len();
+        if has_more {
+            data.pop();
+        }
+
+        let mut edges = data
+            .into_iter()
+            .map(|node| Edge {
+                cursor: node
+                    .serialize_cursor()
+                    .expect("Error while serialize_cursor in assert_read_result"),
+                node,
+            })
+            .collect::<Vec<_>>();
+
+        if self.args.is_backward() {
+            edges = edges.into_iter().rev().collect();
+        }
+
+        let page_info = if self.args.is_backward() {
+            PageInfo {
+                has_previous_page: has_more,
+                start_cursor: edges.first().map(|e| e.cursor.to_owned()),
+                ..Default::default()
+            }
+        } else {
+            PageInfo {
+                has_next_page: has_more,
+                end_cursor: edges.last().map(|e| e.cursor.to_owned()),
+                ..Default::default()
+            }
+        };
+
+        Ok(ReadResult { edges, page_info })
+    }
+}
+
+impl<T> Deref for Reader<T> {
+    type Target = Vec<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for Reader<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
+
+pub trait Bind {
+    type T: Cursor + Clone;
+
+    fn sort_by(data: &mut Vec<Self::T>, is_order_desc: bool);
+    fn retain(
+        data: &mut Vec<Self::T>,
+        cursor: <<Self as Bind>::T as Cursor>::T,
+        is_order_desc: bool,
+    );
+}
+
+impl Bind for crate::Event {
+    type T = Self;
+
+    fn sort_by(data: &mut Vec<Self::T>, is_order_desc: bool) {
+        if !is_order_desc {
+            data.sort_by(|a, b| {
+                if a.timestamp != b.timestamp {
+                    return a.timestamp.cmp(&b.timestamp);
+                }
+
+                if a.version != b.version {
+                    return a.version.cmp(&b.version);
+                }
+
+                a.id.cmp(&b.id)
+            });
+        } else {
+            data.sort_by(|a, b| {
+                if a.timestamp != b.timestamp {
+                    return b.timestamp.cmp(&a.timestamp);
+                }
+
+                if a.version != b.version {
+                    return b.version.cmp(&a.version);
+                }
+
+                b.id.cmp(&a.id)
+            });
+        }
+    }
+
+    fn retain(
+        data: &mut Vec<Self::T>,
+        cursor: <<Self as Bind>::T as Cursor>::T,
+        is_order_desc: bool,
+    ) {
+        data.retain(|event| {
+            if is_order_desc {
+                event.timestamp < cursor.t
+                    || (event.timestamp == cursor.t
+                        && (event.version < cursor.v
+                            || (event.version == cursor.v && event.id < cursor.i)))
+            } else {
+                event.timestamp > cursor.t
+                    || (event.timestamp == cursor.t
+                        && (event.version > cursor.v
+                            || (event.version == cursor.v && event.id > cursor.i)))
+            }
+        });
     }
 }
--- liteventd/tests/cursor.rs
+++ liteventd/tests/cursor.rs
@@ -1,107 +1,265 @@
 use liteventd::{
     Event,
-    cursor::{Args, Cursor, Edge, Order, PageInfo, ReadResult},
+    cursor::{self, Args, Cursor, Order, PageInfo, ReadResult, Reader},
 };
 use rand::{Rng, seq::IndexedRandom};
 use std::collections::HashMap;
 use ulid::Ulid;
 
-// @TODO: Create Vec reader logic in cursor.rs and add static data tests than use it here by reading
-// data vec and replace with vec reader result
+#[test]
+fn forward() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone()).execute()?;
+
+    assert_eq!(res.edges.len(), events.len());
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_next_page: false,
+            end_cursor: res.edges.last().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+
+    assert_eq!(events[3], res.edges[0].node); // 01K14C428V3TNC2K7DCFEJ4HV4 1 1753567001
+    assert_eq!(events[2], res.edges[1].node); // 01K14C49SHZGXFVAKJJTJYQS70 2 1753567361
+    assert_eq!(events[7], res.edges[2].node); // 01K14CA43DENK9KM08REE27EE7 3 1753567361
+    assert_eq!(events[5], res.edges[3].node); // 01K14CAERY65HK16EWV7B0X9V6 5 1753567482
+    assert_eq!(events[9], res.edges[4].node); // 01K14CAPNNDMVZH4RCDCW3WWPS 5 1753567482
+    assert_eq!(events[1], res.edges[5].node); // 01K14CFR387QQCNT4GGGVBSAJV 8 1753567482
+    assert_eq!(events[0], res.edges[6].node); // 01K14CFBVQ9DA4240RZ0V89PQS 23 1753567658
+    assert_eq!(events[6], res.edges[7].node); // 01K14CFJR0GZ18SD2F4J5DXEDD 19 1753567670
+    assert_eq!(events[8], res.edges[8].node); // 01K14CFW3GNN366VM32YEGBT13 88 1753567680
+    assert_eq!(events[4], res.edges[9].node); // 01K14CFZJ0T1KJHBK5084C30BK 52 1753567690
+
+    Ok(())
+}
+
+#[test]
+fn forward_3() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone()).forward(3, None).execute()?;
+
+    assert_eq!(res.edges.len(), 3);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_next_page: true,
+            end_cursor: res.edges.last().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[3], res.edges[0].node); // 01K14C428V3TNC2K7DCFEJ4HV4 1 1753567001
+    assert_eq!(events[2], res.edges[1].node); // 01K14C49SHZGXFVAKJJTJYQS70 2 1753567361
+    assert_eq!(events[7], res.edges[2].node); // 01K14CA43DENK9KM08REE27EE7 3 1753567361
+    Ok(())
+}
+
+#[test]
+fn forward_2_after_3() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone())
+        .forward(2, events[3].serialize_cursor().ok())
+        .execute()?;
+
+    assert_eq!(res.edges.len(), 2);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_next_page: true,
+            end_cursor: res.edges.last().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[2], res.edges[0].node); // 01K14C49SHZGXFVAKJJTJYQS70 2 1753567361
+    assert_eq!(events[7], res.edges[1].node); // 01K14CA43DENK9KM08REE27EE7 3 1753567361
+
+    Ok(())
+}
+
+#[test]
+fn forward_2_after_9() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone())
+        .forward(2, events[9].serialize_cursor().ok())
+        .execute()?;
+
+    assert_eq!(res.edges.len(), 2);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_next_page: true,
+            end_cursor: res.edges.last().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[1], res.edges[0].node); // 01K14CFR387QQCNT4GGGVBSAJV 8 1753567482
+    assert_eq!(events[0], res.edges[1].node); // 01K14CFBVQ9DA4240RZ0V89PQS 23 1753567658
+
+    Ok(())
+}
+
+#[test]
+fn forward_3_after_8() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone())
+        .forward(3, events[8].serialize_cursor().ok())
+        .execute()?;
+    for (i, edge) in res.edges.iter().enumerate() {
+        let pos = events.iter().position(|e| e.id == edge.node.id).unwrap();
+        println!(
+            "assert_eq!(events[{}],res.edges[{}].node); // {} {} {}",
+            pos,
+            i,
+            edge.node.id.to_string(),
+            edge.node.version,
+            edge.node.timestamp
+        );
+    }
+
+    assert_eq!(res.edges.len(), 1);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_next_page: false,
+            end_cursor: res.edges.last().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+
+    assert_eq!(events[4], res.edges[0].node); // 01K14CFZJ0T1KJHBK5084C30BK 52 1753567690
+
+    Ok(())
+}
+
+#[test]
+fn backward_20() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone()).backward(20, None).execute()?;
+
+    assert_eq!(res.edges.len(), events.len());
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_previous_page: false,
+            start_cursor: res.edges.first().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[3], res.edges[0].node); // 01K14C428V3TNC2K7DCFEJ4HV4 1 1753567001
+    assert_eq!(events[2], res.edges[1].node); // 01K14C49SHZGXFVAKJJTJYQS70 2 1753567361
+    assert_eq!(events[7], res.edges[2].node); // 01K14CA43DENK9KM08REE27EE7 3 1753567361
+    assert_eq!(events[5], res.edges[3].node); // 01K14CAERY65HK16EWV7B0X9V6 5 1753567482
+    assert_eq!(events[9], res.edges[4].node); // 01K14CAPNNDMVZH4RCDCW3WWPS 5 1753567482
+    assert_eq!(events[1], res.edges[5].node); // 01K14CFR387QQCNT4GGGVBSAJV 8 1753567482
+    assert_eq!(events[0], res.edges[6].node); // 01K14CFBVQ9DA4240RZ0V89PQS 23 1753567658
+    assert_eq!(events[6], res.edges[7].node); // 01K14CFJR0GZ18SD2F4J5DXEDD 19 1753567670
+    assert_eq!(events[8], res.edges[8].node); // 01K14CFW3GNN366VM32YEGBT13 88 1753567680
+    assert_eq!(events[4], res.edges[9].node); // 01K14CFZJ0T1KJHBK5084C30BK 52 1753567690
+
+    Ok(())
+}
+
+#[test]
+fn backward_3() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone()).backward(3, None).execute()?;
+
+    assert_eq!(res.edges.len(), 3);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_previous_page: true,
+            start_cursor: res.edges.first().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[6], res.edges[0].node); // 01K14CFJR0GZ18SD2F4J5DXEDD 19 1753567670
+    assert_eq!(events[8], res.edges[1].node); // 01K14CFW3GNN366VM32YEGBT13 88 1753567680
+    assert_eq!(events[4], res.edges[2].node); // 01K14CFZJ0T1KJHBK5084C30BK 52 1753567690
+
+    Ok(())
+}
+
+#[test]
+fn backward_2_after_4() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone())
+        .backward(2, events[4].serialize_cursor().ok())
+        .execute()?;
+
+    assert_eq!(res.edges.len(), 2);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_previous_page: true,
+            start_cursor: res.edges.first().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[6], res.edges[0].node); // 01K14CFJR0GZ18SD2F4J5DXEDD 19 1753567670
+    assert_eq!(events[8], res.edges[1].node); // 01K14CFW3GNN366VM32YEGBT13 88 1753567680
+
+    Ok(())
+}
+
+#[test]
+fn backward_2_after_2() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone())
+        .backward(2, events[2].serialize_cursor().ok())
+        .execute()?;
+
+    assert_eq!(res.edges.len(), 1);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_previous_page: false,
+            start_cursor: res.edges.first().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[3], res.edges[0].node); // 01K14C428V3TNC2K7DCFEJ4HV4 1 1753567001
+
+    Ok(())
+}
+
+#[test]
+fn backward_3_after_8() -> anyhow::Result<()> {
+    let events = get_events();
+    let res = Reader::new(events.clone())
+        .backward(3, events[8].serialize_cursor().ok())
+        .execute()?;
+
+    assert_eq!(res.edges.len(), 3);
+    assert_eq!(
+        res.page_info,
+        PageInfo {
+            has_previous_page: true,
+            start_cursor: res.edges.first().map(|e| e.cursor.to_owned()),
+            ..Default::default()
+        }
+    );
+    assert_eq!(events[1], res.edges[0].node); // 01K14CFR387QQCNT4GGGVBSAJV 8 1753567482
+    assert_eq!(events[0], res.edges[1].node); // 01K14CFBVQ9DA4240RZ0V89PQS 23 1753567658
+    assert_eq!(events[6], res.edges[2].node); // 01K14CFJR0GZ18SD2F4J5DXEDD 19 1753567670
+
+    Ok(())
+}
+
 pub fn assert_read_result(
     args: Args,
     order: Order,
-    mut data: Vec<Event>,
+    data: Vec<Event>,
     result: ReadResult<Event>,
 ) -> anyhow::Result<()> {
-    let is_order_desc = matches!(
-        (&order, args.is_backward()),
-        (Order::Asc, true) | (Order::Desc, false)
-    );
+    let data = cursor::Reader::new(data)
+        .args(args)
+        .order(order)
+        .execute()?;
 
-    if !is_order_desc {
-        data.sort_by(|a, b| {
-            if a.timestamp != b.timestamp {
-                return a.timestamp.cmp(&b.timestamp);
-            }
-
-            if a.version != b.version {
-                return a.version.cmp(&b.version);
-            }
-
-            a.id.cmp(&b.id)
-        });
-    } else {
-        data.sort_by(|a, b| {
-            if a.timestamp != b.timestamp {
-                return b.timestamp.cmp(&a.timestamp);
-            }
-
-            if a.version != b.version {
-                return b.version.cmp(&a.version);
-            }
-
-            b.id.cmp(&a.id)
-        });
-    }
-
-    let (limit, cursor) = args.get_info();
-
-    if let Some(cursor) = cursor.as_ref() {
-        let cursor = Event::deserialize_cursor(cursor)?;
-        data.retain(|event| {
-            if is_order_desc {
-                event.timestamp < cursor.t
-                    || (event.timestamp == cursor.t
-                        && (event.version < cursor.v
-                            || (event.version == cursor.v && event.id < cursor.i)))
-            } else {
-                event.timestamp > cursor.t
-                    || (event.timestamp == cursor.t
-                        && (event.version > cursor.v
-                            || (event.version == cursor.v && event.id > cursor.i)))
-            }
-        });
-    }
-
-    let data_len = data.len();
-    data = data.into_iter().take((limit + 1).into()).collect();
-
-    let has_more = data_len > data.len();
-    if has_more {
-        data.pop();
-    }
-
-    let mut edges = data
-        .into_iter()
-        .map(|node| Edge {
-            cursor: node
-                .serialize_cursor()
-                .expect("Error while serialize_cursor in assert_read_result"),
-            node,
-        })
-        .collect::<Vec<_>>();
-
-    if args.is_backward() {
-        edges = edges.into_iter().rev().collect();
-    }
-
-    let page_info = if args.is_backward() {
-        PageInfo {
-            has_previous_page: has_more,
-            start_cursor: edges.first().map(|e| e.cursor.to_owned()),
-            ..Default::default()
-        }
-    } else {
-        PageInfo {
-            has_next_page: has_more,
-            end_cursor: edges.last().map(|e| e.cursor.to_owned()),
-            ..Default::default()
-        }
-    };
-
-    assert_eq!(result.page_info, page_info);
-    assert_eq!(result.edges, edges);
+    assert_eq!(result.page_info, data.page_info);
+    assert_eq!(result.edges, data.edges);
 
     Ok(())
 }
@@ -152,4 +310,33 @@ pub fn get_data() -> Vec<Event> {
     }
 
     data
+}
+
+fn get_events() -> Vec<Event> {
+    vec![
+        create_event("01K14CFBVQ9DA4240RZ0V89PQS", 23, 1753567658),
+        create_event("01K14CFR387QQCNT4GGGVBSAJV", 8, 1753567482),
+        create_event("01K14C49SHZGXFVAKJJTJYQS70", 2, 1753567361),
+        create_event("01K14C428V3TNC2K7DCFEJ4HV4", 1, 1753567001),
+        create_event("01K14CFZJ0T1KJHBK5084C30BK", 52, 1753567690),
+        create_event("01K14CAERY65HK16EWV7B0X9V6", 5, 1753567482),
+        create_event("01K14CFJR0GZ18SD2F4J5DXEDD", 19, 1753567670),
+        create_event("01K14CA43DENK9KM08REE27EE7", 3, 1753567361),
+        create_event("01K14CFW3GNN366VM32YEGBT13", 88, 1753567680),
+        create_event("01K14CAPNNDMVZH4RCDCW3WWPS", 5, 1753567482),
+    ]
+}
+
+fn create_event(id: &str, version: u16, timestamp: u64) -> Event {
+    Event {
+        id: Ulid::from_string(id).unwrap(),
+        name: "MessageSent".to_owned(),
+        aggregate_id: Ulid::new(),
+        aggregate_type: "Message".to_owned(),
+        version,
+        routing_key: "".to_owned(),
+        timestamp,
+        data: Default::default(),
+        metadata: Default::default(),
+    }
 }
